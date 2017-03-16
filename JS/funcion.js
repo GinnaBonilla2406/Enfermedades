a3b4c17eddfe89e11fe44b6607560eda65565bee
GinnaBonilla2406 <g2406b@gmail.com>
2017-03-16 16:14:00 -0500

    funcion.js

---
+++ JS/funcion.js
@@ -0,0 +1,18 @@
+console.log('Funcionandoo')
+function tomar_sintomas()
+{
+	var lista=document.getElementById('select');
+	var salida="";
+	for (var i = 0; i < lista.length; i++) {
+		if (lista.item(i).selected) 
+		{
+			if (salida=="") {
+				salida+= (lista.item(i).value);
+			} else{
+				salida+= "," +(lista.item(i).value) 
+			}
+					}	
+	}
+	document.getElementById('cont-salida').value=salida
+	console.log(salida)
+}
